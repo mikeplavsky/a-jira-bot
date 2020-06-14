2c4fd1fb9874fa1c524eae9f78a723e5451e6315
mikplavsky <mikhail.plavskiy@quest.com>
2020-06-15 01:36:21 +0300

    got first question

--- bot.js
+++ bot.js
@@ -1,28 +1,79 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+const { 
+    ActivityHandler, 
+    MessageFactory } = require('botbuilder');
 
-const { ActivityHandler, MessageFactory } = require('botbuilder');
+const { 
+    ComponentDialog,
+    WaterfallDialog,
+    TextPrompt,
+    DialogTurnStatus,
+    DialogSet } = require('botbuilder-dialogs');
+
+const { 
+    MemoryStorage,
+    ConversationState } = require('botbuilder-core');
+
+const state = (new ConversationState( 
+    new MemoryStorage())).createProperty('dialogState');
+
+class MainDlg extends ComponentDialog {
+    constructor() {
+        super('MainDlg');
+
+        this.addDialog( new WaterfallDialog( 'start',[
+
+            async (step) => {
+                return await step.prompt(
+                    'namePrompt',
+                    'What is your name?');
+            }
+
+        ]));
+
+        this.addDialog( new TextPrompt( 'namePrompt' ));
+    }
+} 
 
 class EchoBot extends ActivityHandler {
+
     constructor() {
+
         super();
-        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-        this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text }`;
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
-            // By calling next() you ensure that the next BotHandler is run.
+        const dlg = new MainDlg();
+
+        this.onMessage( async (context, next) => {
+
+            const dlgSet = new DialogSet(state);
+            dlgSet.add(dlg);
+
+            const ctxt = await dlgSet.createContext(context);
+            const res = await ctxt.continueDialog();
+
+            console.log(res);
+
+            if (res.status == DialogTurnStatus.empty) {
+               const res1 = await ctxt.beginDialog(dlg.id);
+               console.log(res1);
+            }
+
             await next();
+
         });
 
         this.onMembersAdded(async (context, next) => {
+
             const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome!';
+            const welcomeText = 'Hi, there!';
+
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+                    await context.sendActivity(
+                        MessageFactory.text(
+                            welcomeText,
+                            welcomeText));
                 }
             }
-            // By calling next() you ensure that the next BotHandler is run.
+
             await next();
         });
     }
